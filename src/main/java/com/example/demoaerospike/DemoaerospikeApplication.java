1935ba5bb5931afc0dc4bc9a36d5cdd4a7c6ff96
Syed Ateebul Islam <32341313+syedateebulislam@users.noreply.github.com>
2023-10-28 17:21:49 +0530

    test commit

---
+++ src/main/java/com/example/demoaerospike/DemoaerospikeApplication.java
@@ -0,0 +1,15 @@
+package com.example.demoaerospike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoaerospikeApplication {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(DemoaerospikeApplication.class, args);
+	}
+
+}
